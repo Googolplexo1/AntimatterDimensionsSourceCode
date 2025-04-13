249af847ccfbf017e982cceec77396605f8b7483
Googolplexo <i.a.kokarev@yandex.ru>
2025-04-13 14:00:10 +0500

    .

--- src/components/tabs/time-studies/TimeStudiesTab.vue
+++ src/components/tabs/time-studies/TimeStudiesTab.vue
@@ -124,10 +124,10 @@ export default {
     },
     exportStudyTree() {
       if (player.timestudy.studies.length === 0) {
-        GameUI.notify.error("You cannot export an empty Time Study Tree!");
+        GameUI.notify.error("Невозможно экспортировать пустое Древо Исследований!");
       } else {
         copyToClipboard(GameCache.currentStudyTree.value.exportString);
-        GameUI.notify.info("Exported current Time Studies to your clipboard");
+        GameUI.notify.info("Древо Исследований экспортировано в буфер обмена");
       }
     }
   }
